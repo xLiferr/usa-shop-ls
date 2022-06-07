07f591fd90bc856d68d443e6b2a0af6018d55c8b
xRaziel <73146905+xRaziel@users.noreply.github.com>
2022-06-06 23:34:24 -0400

    Register modal
    Modal del registro, aún falta completar que se deslice la pantalla

---
+++ Frontend/src/components/RegisterModal/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./style.css";
+import logo from "../../images/logo.png";
+import close from "../../images/close.png";
+
+export const RegisterModal = () => {
+    return(
+        <div className="rm-bg">
+            <div className="rm-border">
+                <div className="rm-content">
+                    <div className="rm-close"><img src={close} alt=""/></div>
+                    <div className="rm-header">
+                        <div className="rm-header-title"><h3>¡Bienvenido!</h3></div>
+                        <div className="rm-header-logo"><img src={logo} alt=""/></div>
+                    </div>
+                    <form className="rm-form">
+                        <div className="rm-input">
+                            <input type="text" required />
+                            <label>Correo electrónico</label>
+                        </div>
+                        <div className="rm-input">
+                            <input type="password" required />
+                            <label>Contraseña</label>
+                        </div>
+                        <div className="rm-input">
+                            <input type="password" required />
+                            <label>Confirmar contraseña</label>
+                        </div>
+                        <div className="rm-input">
+                            <input type="text" required />
+                            <label>Nombre</label>
+                        </div>
+                        <div className="rm-input">
+                            <input type="text" required />
+                            <label>Apellidos</label>
+                        </div>
+                        <div className="rm-input">
+                            <input type="text" required />
+                            <label>RUT</label>
+                        </div>
+                        <div className="rm-input">
+                            <input type="text" required />
+                            <label>Teléfono</label>
+                        </div>
+                    </form>
+                    <div className="rm-buttons">
+                        <div className="rm-button"><button type="submit">Registrar</button></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- Frontend/src/pages/Home/index.js
+++ Frontend/src/pages/Home/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
 import { Categorys } from '../../components/Categorys'
-{/*import { LoginModal } from '../../components/LoginModal'*/}
+import { LoginModal } from '../../components/LoginModal'
+import { RegisterModal } from '../../components/RegisterModal'
 
 export const Home = () => {
   
@@ -10,7 +11,8 @@ export const Home = () => {
     <div>
       <Header/>
       <Categorys/>
-      {/*<LoginModal/>*/}
+      {/*<LoginModal/>
+      <RegisterModal/>*/}
       <Footer />
     </div>
   );
