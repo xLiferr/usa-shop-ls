1bd6272614dede16f42a9c2b439dd19071afda2b
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-06-13 12:31:30 -0400

    Menu web admin

---
+++ Frontend/src/components/SidebarWA/index.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+// Icons
+import logo from "../../images/logo.png";
+import menuOpen from "../../images/menuOpen.png";
+import menuClose from "../../images/menuClose.png";
+import addProducts from "../../images/add-products.png";
+import categories from "../../images/categories.png";
+import clients from "../../images/clients.png";
+import orders from "../../images/orders.png";
+import products from "../../images/products.png";
+import sales from "../../images/sales.png";
+import back from "../../images/back.png";
+// Components
+
+
+export const SidebarWA = () => {
+  const [waSidebar, setWaSidebar] = useState(false);
+  return (
+    <>
+      <div className={!waSidebar ? ("wa-sidebar-open") : ("wa-sidebar-close")}>
+        <div className="wa-sidebar-header" onClick={() => setWaSidebar(!waSidebar)}>
+          <div className="wa-sidebar-logo"><img src={logo} alt="" /></div>
+          <div className="wa-sidebar-title">Web Admin</div>
+          {waSidebar 
+          ? (<img className="wa-menu-open wa-sidebar-icon" src={menuOpen} alt="" onClick={() => setWaSidebar(!waSidebar)}/>) 
+          : (<img className="wa-menu-close wa-sidebar-icon" src={menuClose} alt="" onClick={() => setWaSidebar(!waSidebar)}/>)}          
+        </div>
+        <ul className="wa-sidebar-items">
+          <li className="wa-sidebar-item">
+            <Link className="wa-sidebar-link" to="/web-admin">
+              <img className="wa-sidebar-icon" src={sales} alt="" />
+              Reporte de ventas
+            </Link>
+          </li>
+          <li className="wa-sidebar-item">
+            <Link className="wa-sidebar-link" to="/wa-agregar-producto">
+              <img className="wa-sidebar-icon" src={addProducts} alt="" />
+              Agregar un producto
+            </Link>
+          </li>
+          <li className="wa-sidebar-item">
+            <Link className="wa-sidebar-link" to="/wa-productos">
+              <img className="wa-sidebar-icon" src={products} alt="" />
+              Productos
+            </Link>
+          </li>
+          <li className="wa-sidebar-item">
+            <Link className="wa-sidebar-link" to="/wa-categorias">
+              <img className="wa-sidebar-icon" src={categories} alt="" />
+              Categorías
+            </Link>
+          </li>
+          <li className="wa-sidebar-item">
+            <Link className="wa-sidebar-link" to="/wa-pedidos">
+              <img className="wa-sidebar-icon" src={orders} alt="" />
+              Pedidos
+            </Link>
+          </li>
+          <li className="wa-sidebar-item">
+            <Link className="wa-sidebar-link" to="/wa-clientes">
+              <img className="wa-sidebar-icon" src={clients} alt="" />
+              Clientes
+            </Link>
+          </li>
+          <li className="wa-sidebar-item">
+            <Link className="wa-sidebar-link" to="/">
+              <img className="wa-sidebar-icon" src={back} alt="" />
+              Volver a la tienda
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
