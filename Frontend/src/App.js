d39353a961db53c1c64cc2563e0a039af1856142
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-06-13 12:45:49 -0400

    Cambio add products

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,7 +3,7 @@ import { Home } from './pages/Home';
 
 // WEB ADMIN
 import { WebAdmin } from './pages/WebAdmin';
-import { AddProduct } from './pages/WebAdmin/AddProduct';
+import { WAAddProduct } from './pages/WebAdmin/WAAddProduct';
 import { WAProducts } from './pages/WebAdmin/WAProducts';
 import { WACategories } from './pages/WebAdmin/WACategories';
 import { WAOrders } from './pages/WebAdmin/WAOrders';
@@ -18,7 +18,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/web-admin" element={<WebAdmin />}></Route>
-          <Route path="/wa-agregar-producto" element={<AddProduct />}></Route>
+          <Route path="/wa-agregar-producto" element={<WAAddProduct />}></Route>
           <Route path="/wa-productos" element={<WAProducts />}></Route>
           <Route path="/wa-categorias" element={<WACategories />}></Route>
           <Route path="/wa-pedidos" element={<WAOrders />}></Route>
