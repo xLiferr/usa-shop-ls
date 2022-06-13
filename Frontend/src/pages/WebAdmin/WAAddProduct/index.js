6b4eafda99ef61444502831a64ba60b9f1e5d59c
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-06-13 12:43:53 -0400

    Pagina Agregar producto

--- Frontend/src/pages/WebAdmin/WAAddProduct/index.js
+++ Frontend/src/pages/WebAdmin/WAAddProduct/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 import { SidebarWA } from "../../../components/SidebarWA";
 import { Footer } from "../../../components/Footer";
 
-export const AddProduct = () => {
+export const WAAddProduct = () => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [category, setCategory] = useState("");
@@ -12,43 +12,43 @@ export const AddProduct = () => {
   return(
     <>
       <SidebarWA />
-      <div className="ap-page">
-        <div className="ap-header">
+      <div className="wa-ap-page">
+        <div className="wa-ap-header">
           <h3>Agregar un producto</h3>
         </div>
-        <div className="ap-content">
-          <form className="ap-form">
-            <div className="ap-form-item">
+        <div className="wa-ap-content">
+          <form className="wa-ap-form">
+            <div className="wa-ap-form-item">
               <h4>Nombre</h4>
               <input type="text" onChange={(e) => setName(e.target.value)} required />
             </div>
-            <div className="ap-form-item">
+            <div className="wa-ap-form-item">
               <h4>Descripción</h4>
               <textarea placeholder="Escriba la descripción de producto" onChange={(e) => setDescription(e.target.value)} required/>
             </div>
-            <div className="ap-form-item">
+            <div className="wa-ap-form-item">
               <h4>Categoría</h4>
               <select>
                 <option>Algo</option>
               </select>
             </div>
-            <div className="ap-form-item">
+            <div className="wa-ap-form-item">
               <h4>Stock</h4>
               <input type="number" min="1" defaultValue="1" onChange={(e) => setStock(e.target.value)} required />
             </div>
-            <div className="ap-form-item">
+            <div className="wa-ap-form-item">
               <h4>Precio</h4> 
               <input type="text" required onChange={(e) => setPrice(e.target.value)} placeholder="Ej: 19.999"/>
             </div>
-            <div className="ap-form-item">
+            <div className="wa-ap-form-item">
               <h4>Fotos</h4>
               <input type="file" multiple required/>
             </div>
-            <div className="ap-form-buttons">
+            <div className="wa-ap-form-buttons">
               <input type="submit" value="Guardar" />
             </div>
           </form>
-          <div className="ap-preview">
+          <div className="wa-ap-preview">
             
           </div>
         </div>
--- Frontend/src/pages/WebAdmin/index.js
+++ Frontend/src/pages/WebAdmin/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./style.css";
 import { SidebarWA } from "../../components/SidebarWA";
+import { Footer } from "../../components/Footer";
 
 export const WebAdmin = () => {
   return (
-    <SidebarWA />
+    <>
+      <SidebarWA />
+      <Footer />
+    </>
   );
 }
