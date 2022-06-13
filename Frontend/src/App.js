f36804ae62c57c1baeea27ba4a5a212045525c74
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-06-13 12:34:15 -0400

    Rutas web admin (falta proteger)

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
+
+// WEB ADMIN
 import { WebAdmin } from './pages/WebAdmin';
+import { AddProduct } from './pages/WebAdmin/AddProduct';
+import { WAProducts } from './pages/WebAdmin/WAProducts';
+import { WACategories } from './pages/WebAdmin/WACategories';
+import { WAOrders } from './pages/WebAdmin/WAOrders';
+import { WAClients } from './pages/WebAdmin/WAClients';
+
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/web-admin" element={<WebAdmin />}></Route>
-      </Routes>
-    </Router>
+    <div className='usa-shop'>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/web-admin" element={<WebAdmin />}></Route>
+          <Route path="/wa-agregar-producto" element={<AddProduct />}></Route>
+          <Route path="/wa-productos" element={<WAProducts />}></Route>
+          <Route path="/wa-categorias" element={<WACategories />}></Route>
+          <Route path="/wa-pedidos" element={<WAOrders />}></Route>
+          <Route path="/wa-clientes" element={<WAClients />}></Route>
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
