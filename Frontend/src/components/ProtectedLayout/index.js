155f49d8d9c6a553084b2fdd3e928fd11896027a
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-06-30 11:44:30 -0400

    Componente protector de rutas
    Proteger rutas por rol

---
+++ Frontend/src/components/ProtectedLayout/index.js
@@ -0,0 +1,16 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+
+export const ProtectedLayout = ({ allowedRoles }) => {
+  const { user } = useAuth();
+  const check = () => {
+    try {
+      if (user.access_token && user.email && allowedRoles.find((role) =>  user.role.includes(role))) return <Outlet />; 
+      else return <Navigate to="/" />;   
+    } catch (error) {
+      return <Navigate to="/" />;
+    }
+  }
+  return (check())
+}
+
