405624d49817e8c28fc36462ca903152a4c360ce
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-06-30 11:46:42 -0400

    Cambios en el App.js
    - Contexto del Auth para toda la aplicación, ahora se puede obtener el usuario en todos lados con el hook useAuth.
    
    - Rutas del web admin protegidas con el rol

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,21 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './pages/Home';
-
-// WEB ADMIN
+import { AuthProvider } from './context/AuthProvider';
+import { ProtectedLayout } from './components/ProtectedLayout';
+// Web Admin routes
 import { WebAdmin } from './pages/WebAdmin';
 import { WAAddProduct } from './pages/WebAdmin/WAAddProduct';
 import { WAProducts } from './pages/WebAdmin/WAProducts';
 import { WACategories } from './pages/WebAdmin/WACategories';
 import { WAOrders } from './pages/WebAdmin/WAOrders';
 import { WAClients } from './pages/WebAdmin/WAClients';
-
-//CATEGORIAS
-
+// Public routes
+import { Home } from './pages/Home';
 import { Accesorios } from './pages/Accesorios';
 import { Carteras } from './pages/Carteras';
 import { Hombre } from './pages/Hombre';
 import { Mochilas } from './pages/Mochilas';
-import { Mujer } from './pages/Mujer'; 
+import { Mujer } from './pages/Mujer';
 
 import "./App.css";
 
@@ -23,20 +22,24 @@ function App() {
   return (
     <div className='usa-shop'>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/web-admin" element={<WebAdmin />}></Route>
-          <Route path="/wa-agregar-producto" element={<WAAddProduct />}></Route>
-          <Route path="/wa-productos" element={<WAProducts />}></Route>
-          <Route path="/wa-categorias" element={<WACategories />}></Route>
-          <Route path="/wa-pedidos" element={<WAOrders />}></Route>
-          <Route path="/wa-clientes" element={<WAClients />}></Route>
-          <Route path="/accesorios" element={<Accesorios />}></Route>
-          <Route path="/carteras" element={<Carteras />}></Route>
-          <Route path="/hombre" element={<Hombre />}></Route>
-          <Route path="/mochilas" element={<Mochilas />}></Route>
-          <Route path="/mujer" element={<Mujer />}></Route>
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/accesorios" element={<Accesorios />}></Route>
+            <Route path="/carteras" element={<Carteras />}></Route>
+            <Route path="/hombre" element={<Hombre />}></Route>
+            <Route path="/mochilas" element={<Mochilas />}></Route>
+            <Route path="/mujer" element={<Mujer />}></Route>
+            <Route element={<ProtectedLayout allowedRoles={["Administrador"]}/>}>
+              <Route path="/web-admin" element={<WebAdmin />}></Route>
+              <Route path="/wa-agregar-producto" element={<WAAddProduct />}></Route>
+              <Route path="/wa-productos" element={<WAProducts />}></Route>
+              <Route path="/wa-categorias" element={<WACategories />}></Route>
+              <Route path="/wa-pedidos" element={<WAOrders />}></Route>
+              <Route path="/wa-clientes" element={<WAClients />}></Route>
+            </Route>
+          </Routes>
+        </AuthProvider>
       </Router>
     </div>
   );
