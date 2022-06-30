17714e15b6d9400b813bfb51be77d26b06e2eab8
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-06-30 11:43:15 -0400

    Login completo

--- Frontend/src/components/LoginModal/index.js
+++ Frontend/src/components/LoginModal/index.js
@@ -1,50 +1,90 @@
-import React from "react";
-import {useEffect, useState} from 'react';
+import React, { useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
+import axios from "axios";
+import Swal from "sweetalert2";
 import "./style.css";
+// Components
+import { RegisterModal } from '../RegisterModal';
+// Icons
 import logo from "../../images/logo.png";
 import close from "../../images/close.png";
-import {RegisterModal} from '../RegisterModal';
-
-
-
 
 export const LoginModal = ({ closeModal }) => {
-  const [openModal,setOpenModal] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useAuth();
+
+  const confirmLogin = async (access_token, role) => {
+    await login({ email, password, role, access_token });
+    setEmail("");
+    setPassword("");
+    Swal.fire({
+      title: 'Sesión iniciada!',
+      text: 'Usted ha iniciado sesión correctamente.',
+      icon: 'success',
+      showConfirmButton: false,
+      timer: 2000
+    })
+    closeModal(false);
+  }
+
+  const denyLogin = () => {
+    Swal.fire({
+      title: 'Sesión no iniciada',
+      text: 'Su correo electrónico y/o contraseña no coinciden.',
+      icon: 'error',
+      confirmButtonText: 'Continuar',
+      confirmButtonColor: '#00AFB9',
+    })
+  }
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3001/auth/login', {
+      username: email,
+      password: password
+    }).then((response) => {
+      if (response.status === 201) confirmLogin(response.data.access_token, "Administrador");
+    }).catch((error) => {
+      if (error.response.status === 401) denyLogin();
+    })
+  }
 
   return (
     <div className="lm-bg">
       <div className="lm-border">
         <div className="lm-content">
           <button className="lm-close" onClick={() => closeModal(false)} >
-            <img src={close} alt=""/>
+            <img src={close} alt="" />
           </button>
           <div className="lm-header">
             <div className="lm-header-title"><h3>¡Bienvenido!</h3></div>
-            <div className="lm-header-logo"><img src={logo} alt=""/></div>
+            <div className="lm-header-logo"><img src={logo} alt="" /></div>
           </div>
-          <form className="lm-form">
+          <form className="lm-form" onSubmit={handleLogin}>
             <div className="lm-input">
-              <input type="text" required />
+              <input type="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
               <label>Correo electrónico</label>
             </div>
             <div className="lm-input">
-              <input type="password" required />
+              <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
               <label>Contraseña</label>
             </div>
-          </form>
-          <div className="lm-buttons">
-            <div className="lm-button"><button type="submit">Ingresar</button></div>
-            <div className="lm-others">
-              <button>¿Olvidaste tu contraseña?</button>
-              <button className="openModal"
-                    onClick={() => {
-                        setOpenModal(true);
-                    }}>
+            <div className="lm-buttons">
+              <div className="lm-button"><button type="submit">Ingresar</button></div>
+              <div className="lm-others">
+                <button>¿Olvidaste tu contraseña?</button>
+                <button className="openModal"
+                  onClick={() => {
+                    setOpenModal(true);
+                  }}>
                   Registrarse
                 </button>
-                {openModal && <RegisterModal closeModal={setOpenModal}/>}
+                {openModal && <RegisterModal closeModal={setOpenModal} />}
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
