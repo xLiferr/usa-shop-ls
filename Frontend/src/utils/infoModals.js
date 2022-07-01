47300db59c01dc2d28c3dfee5da28c6b9ecec70e
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-07-01 12:02:53 -0400

    Timer para el modal de exito

--- Frontend/src/utils/infoModals.js
+++ Frontend/src/utils/infoModals.js
@@ -1,12 +1,13 @@
 import Swal from "sweetalert2";
 
-export const successModal = (title, text, reload) => {
-  Swal.fire({
+export const successModal = async (title, text, reload, timer) => {
+  await Swal.fire({
     title: title,
     text: text,
     icon: 'success',
     confirmButtonText: 'Continuar',
-    confirmButtonColor: '#00AFB9'
+    confirmButtonColor: '#00AFB9',
+    timer: timer
   }).then((result) => {
     if (reload && result.isConfirmed) window.location.reload();
   })
