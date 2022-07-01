df9f90f9284e2e1ba84ba001fe656beff5676bbc
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-07-01 12:42:00 -0400

    Home despliega productos

--- Frontend/src/pages/Home/index.js
+++ Frontend/src/pages/Home/index.js
@@ -1,72 +1,92 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./style.css";
+// Components
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
 import { Categorys } from '../../components/Categorys'
 import { Carousel } from '../../components/Carousel'
+import { ProductCard } from "../../components/ProductCard";
 
 import icon from '../../images/delete.png';
 
 export const Home = () => {
-  
-  return (
-    <div className="usa-shop">
-      <Header/>
-      <Categorys/>
-      <div className="home-content">
-        {/*<Carousel products={[
-          {
-            id: 1,
-            name: "Mochila",
-            description: "Mochila para vos",
-            images: [icon],
-            price: 19990
-          },
-          {
-            id: 1,
-            name: "Mochila",
-            description: "Mochila para vos",
-            images: [icon],
-            price: 19990
-          },
-          {
-            id: 1,
-            name: "Mochila",
-            description: "Mochila para vos",
-            images: [icon],
-            price: 19990
-          },
-          {
-            id: 1,
-            name: "Mochila",
-            description: "Mochila para vos",
-            images: [icon],
-            price: 19990
-          },
-          {
-            id: 1,
-            name: "Mochila",
-            description: "Mochila para vos",
-            images: [icon],
-            price: 19990
-          },
-          {
-            id: 1,
-            name: "Mochila",
-            description: "Mochila para vos",
-            images: [icon],
-            price: 19990
-          },
-          {
-            id: 1,
-            name: "Mochila",
-            description: "Mochila para vos",
-            images: [icon],
-            price: 19990
-          }
-        ]}/>*/}
-      </div>
-      <Footer />
-    </div>
-  );
-}
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    setProducts([
+      {
+        id: 1,
+        name: "Mochila",
+        description: "Mochila para vos",
+        images: [icon],
+        price: 19990
+      },
+      {
+        id: 1,
+        name: "Mochila",
+        description: "Mochila para vos",
+        images: [icon],
+        price: 19990
+      },
+      {
+        id: 1,
+        name: "Mochila",
+        description: "Mochila para vos",
+        images: [icon],
+        price: 19990
+      },
+      {
+        id: 1,
+        name: "Mochila",
+        description: "Mochila para vos",
+        images: [icon],
+        price: 19990
+      },
+      {
+        id: 1,
+        name: "Mochila",
+        description: "Mochila para vos",
+        images: [icon],
+        price: 19990
+      },
+      {
+        id: 1,
+        name: "Mochila",
+        description: "Mochila para vos",
+        images: [icon],
+        price: 19990
+      },
+      {
+        id: 1,
+        name: "Mochila",
+        description: "Mochila para vos",
+        images: [icon],
+        price: 19990
+      }
+    ])
+    /*
+    axios.get('http://localhost:3001/products/all').then((response) => {
+      if (response.status === 200) setProducts(response.data);
+    }).catch(() => setProducts([]))
+    */
+  }, [])
+    return (
+      <>
+        <Header />
+        <Categorys />
+        <div className="home-section">
+          <div className="home-title"><h2>Productos Destacados</h2></div>
+          <Carousel products={products} />
+        </div>
+        <div className="home-section">
+          <div className="home-title"><h2>Nuevos Productos</h2></div>
+          <div className="home-products">
+            {products.map((product, key) => {
+              return <div className="home-product"><ProductCard key={key} product={product} /></div>
+            })}
+          </div>
+        </div>
+        <Footer />
+      </>
+    );
+  }
