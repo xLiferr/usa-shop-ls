ac96f70f5a82ea3beb8531875a5045fac008a798
xRaziel <felipe.varas01@alumnos.ucn.cl>
2022-07-02 13:52:17 -0400

    Componente de productos ahora con map
    Me falta agregarle el filtro y centrar bien el producto

--- Frontend/src/components/Productos/index.js
+++ Frontend/src/components/Productos/index.js
@@ -3,68 +3,83 @@ import "./style.css";
 import MGN from "../../images/MGN.jpg";
 
 export const Productos = () => {
+
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+  let componentMounted = true;
+
+  useEffect(() => {
+      const getProducts = async() => {
+          setLoading(true);
+          const response = await fetch("http://fakestoreapi.com/products");
+          if(componentMounted){
+              setData(await response.clone().json());
+              setFilter(await response.json());
+              setLoading(false);
+              console.log(filter);
+          }
+
+          return () =>{
+              componentMounted = false;
+          }
+      }
+      getProducts();
+  }, [])
+
+  const Loading = () => {
+    return(
+      <>
+        Loading....
+      </>
+    );
+  }
+
+  const ShowProducts = () => {
+    return(
+      <>
+      <div className="buttons-filter">
+        <button className="btn-filtro">Polerones</button>
+        <button className="btn-filtro">Poleras</button>
+        <button className="btn-filtro">Ropa interior</button>
+      </div>
+      <div className="products-list">
+      {filter.map((product)=>{
+        return(
+          <>
+              <div className="producto">
+                <a href="#">
+                  <div className="producto-img">
+                    <img src={product.image} alt={product.title} />
+                  </div>
+                </a>
+                <div className="producto-footer">
+                  <h1> {product.title} </h1>
+                  <p> Categoria </p>
+                  <p className="precio"> ${product.price} </p>
+                </div>
+                <div className="buttom">
+                  <button className="btn">Añadir al carrito</button>
+                <div>
+                <a href="#" className="btn">
+                  Ver producto
+                </a>
+              </div>
+            </div>
+        </div>
+          </>
+        )
+        
+      })}
+      </div>
+      </>
+    );
+  }
+
   return (
+    
     <div className="productos">
-      <div className="producto">
-        <a href="#">
-          <div className="producto-img">
-            <img src={MGN} alt="" />
-          </div>
-        </a>
-        <div className="producto-footer">
-          <h1> Title </h1>
-          <p> Categoria </p>
-          <p className="precio"> $59.990 </p>
-        </div>
-        <div className="buttom">
-          <button className="btn">Añadir al carrito</button>
-          <div>
-            <a href="#" className="btn">
-              Ver producto
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="producto">
-        <a href="#">
-          <div className="producto-img">
-            <img src={MGN} alt="" />
-          </div>
-        </a>
-        <div className="producto-footer">
-          <h1> Title </h1>
-          <p> Categoria </p>
-          <p className="precio"> $59.990 </p>
-        </div>
-        <div className="buttom">
-          <button className="btn">Añadir al carrito</button>
-          <div>
-            <a href="#" className="btn">
-              Ver producto
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="producto">
-        <a href="#">
-          <div className="producto-img">
-            <img src={MGN} alt="" />
-          </div>
-        </a>
-        <div className="producto-footer">
-          <h1> Title </h1>
-          <p> Categoria </p>
-          <p className="precio"> $59.990 </p>
-        </div>
-        <div className="buttom">
-          <button className="btn">Añadir al carrito</button>
-          <div>
-            <a href="#" className="btn">
-              Ver producto
-            </a>
-          </div>
-        </div>
-      </div>
+      {loading ? <Loading/> : <ShowProducts/>}
     </div>
   );
 };
