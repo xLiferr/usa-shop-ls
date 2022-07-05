b9ce583e766254a7975e032e8b0f59e5f41252b3
Nicolás Pino Silva <64236969+nicoolasps@users.noreply.github.com>
2022-07-05 16:39:54 -0400

    Modificar y eliminar categoria completo

--- Frontend/src/pages/WebAdmin/WACategories/index.js
+++ Frontend/src/pages/WebAdmin/WACategories/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Swal from "sweetalert2";
 import { useAuth } from "../../../hooks/useAuth";
 import "./style.css";
 // Components
@@ -10,115 +9,35 @@ import { SidebarWA } from "../../../components/SidebarWA";
 import editIcon from "../../../images/edit.png";
 import deleteIcon from "../../../images/delete.png";
 // Utils
-import { successModal, errorModal } from "../../../utils/infoModals";
+import { successModal, errorModal, inputModal, askModal } from "../../../utils/infoModals";
 
 export const WACategories = () => {
   const [categories, setCategories] = useState([]);
   const [newCategory, setNewCategory] = useState("");
   const { user } = useAuth();
-  const editHandler = (category) => {
-    console.log("edit")
-    /*
-    Swal.fire({
-      title: 'Modificar categoría',
-      input: 'text',
-      inputLabel: 'Nombre',
-      inputValue: category.name,
-      showCancelButton: true,
-      confirmButtonText: 'Guardar',
-      confirmButtonColor: '#00AFB9',
-      showLoaderOnConfirm: true,
-      inputValidator: (name) => {
-        if(!name) return 'Debes que asignarle un nombre a la categoría.'
-      }
-    }).then((result) => {
-      if (result.isConfirmed) {
-        axios.put("", {
-          category: {
-            id: category.id,
-            name: result.value
-          }
-        }).then((response) => {
-          if (response.status === 200) {
-            Swal.fire({
-              title: 'Categoría modificada!',
-              text: 'La categoría se ha modificado correctamente.',
-              icon: 'success',
-              confirmButtonText: 'Continuar',
-              confirmButtonColor: '#00AFB9',
-            })
-          } else if (response.status >= 400) {
-            Swal.fire({
-              title: 'Error inesperado!',
-              text: 'Hubo un error al intentar modificar la categoría, Inténtelo nuevamente.',
-              icon: 'error',
-              confirmButtonText: 'Continuar',
-              confirmButtonColor: '#00AFB9',
-            })
-          }
-        }).catch((error) => {
-          Swal.fire({
-            title: 'Error inesperado!',
-            text: 'Hubo un error al intentar modificar la categoría, Inténtelo nuevamente',
-            icon: 'error',
-            confirmButtonText: 'Continuar',
-            confirmButtonColor: '#00AFB9',
-          })
-        })
-      }
-    })
-  */
+
+  const editHandler = async (category) => {
+    const result = await inputModal('Modificar categoría', 'text', 'Nombre', category.name, 'Guardar', 'Debes que asignarle un nombre a la categoría.');
+    if (result.isConfirmed) {
+      axios.put(`http://localhost:3001/category/${category.id}`, { access_token: user.access_token, name: result.value }).then((response) => {
+        if (response.status === 200) successModal('Categoría modificada!', 'La categoría se ha modificado correctamente.', true);
+      }).catch(() => {
+        errorModal('Error inesperado!', 'Hubo un error al intentar modificar la categoría, Inténtelo nuevamente');
+      })
+    }
   }
 
-  const deleteHandler = (category) => {
-    console.log("delete")
-    /*
-    Swal.fire({
-      title: '¿Eliminar categoría?',
-      text: 'Si eliminas la categoría, se eliminará de forma permanente de la tienda.',
-      icon: 'warning',
-      showCancelButton: true,
-      cancelButtonColor: '#B1B1B1',
-      cancelButtonText: 'No, mantener categoría',
-      confirmButtonColor: '#d33',
-      confirmButtonText: 'Sí, eliminar categoría'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        axios.delete('').then((response) => {
-          if (response.status === 200) {
-            Swal.fire({
-              title: 'Categoría eliminada!',
-              text: 'La categoría se ha eliminada correctamente.',
-              icon: 'success',
-              confirmButtonText: 'Continuar',
-              confirmButtonColor: '#00AFB9',
-            })
-          } else if (response.status >= 400) {
-            Swal.fire({
-              title: 'Error inesperado!',
-              text: 'Hubo un error al intentar eliminar la categoría, Inténtelo nuevamente.',
-              icon: 'error',
-              confirmButtonText: 'Continuar',
-              confirmButtonColor: '#00AFB9',
-            })
-          }
-        }).catch((error) => {
-          Swal.fire({
-            title: 'Error inesperado!',
-            text: 'Hubo un error al intentar eliminar la categoría, Inténtelo nuevamente.',
-            icon: 'error',
-            confirmButtonText: 'Continuar',
-            confirmButtonColor: '#00AFB9',
-          })
-        })
-      }
-    })
-    */
+  const deleteHandler = async (category) => {
+    const result = await askModal('¿Eliminar categoría?', 'Si eliminas la categoría, se eliminará de forma permanente de la tienda.', 'No, mantener categoría', 'Sí, eliminar categoría');
+    if (result) {
+      axios.delete(`http://localhost:3001/category/${category.id}`, { access_token: user.access_token }).then((response) => {
+        if (response.status === 200) successModal('Categoría eliminada!', 'La categoría se ha eliminada correctamente.', true);
+      }).catch(() => errorModal('Error inesperado!', 'Hubo un error al intentar eliminar la categoría, Inténtelo nuevamente.'));
+    }
   }
 
   const addCategory = (event) => {
     event.preventDefault();
-    console.log("a")
     axios.post("http://localhost:3001/category/create", { access_token: user.access_token, name: newCategory }).then((response) => {
       if (response.status === 201)  {
         successModal('Categoría agregada!', 'La categoría se ha agregado correctamente.', true);
