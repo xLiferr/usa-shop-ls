fa77e3adbb325ee2863eecdf4c40c0f94c3d8f90
xRaziel <73146905+xRaziel@users.noreply.github.com>
2022-07-08 02:00:47 -0400

    Funcionalidad carrito listo
    Falta estilizarlo mejor

--- Backend/dist/main.js
+++ Backend/dist/main.js
@@ -23,7 +23,7 @@ async function bootstrap() {
     console.log(sign.toString());
     console.log("el 2");
     console.log(sign2.toString());
-    await app.listen(3000);
+    await app.listen(3001);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
